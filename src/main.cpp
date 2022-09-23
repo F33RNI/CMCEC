a02d5613bee72139ddb999f5b9d2db6fdbfe82ce
F33RNI <xxoinvizionxx@gmail.com>
2022-09-24 02:54:57 +0300

    Add clock output, modes and control button

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,9 @@
 // Arduino library
 #include <Arduino.h>
 
+// EEPROM library to store settings (mode and clock divider)
+#include <EEPROM.h>
+
 // SPI library
 #include <SPI.h>
 
@@ -40,6 +43,8 @@ const uint8_t PIN_GATE_1 PROGMEM = 5;
 const uint8_t PIN_TRIG_2 PROGMEM = 6;
 const uint8_t PIN_GATE_2 PROGMEM = 7;
 const uint8_t PIN_WS_LED PROGMEM = 3;
+const uint8_t PIN_BTN PROGMEM = A0;
+const uint8_t PIN_CLK_OUT PROGMEM = A1;
 
 // Measured real 1.1V reference in mV
 const uint32_t VREF_ACTUAL_MV PROGMEM = 1099;
@@ -68,7 +73,10 @@ const float GAIN_2 PROGMEM = 1.83076190;
 #define NOTE_START 60 // C4
 #define NOTE_MIN_MV 0
 #define NOTE_MAX_MV 7250
-#define TRIG_PULSE_MS 20
+#define PULSE_DURATION_MS 20
+#define LED_SHOW_STATE_DURATION_MS 500
+#define BTN_SHORT_DURATION_MS 100
+#define BTN_LONG_DURATION_MS 700
 #define MIDI_CHANNEL_1 1
 #define MIDI_CHANNEL_2 2
 #define NOTE_COLOR_LOW 0
@@ -76,6 +84,22 @@ const float GAIN_2 PROGMEM = 1.83076190;
 #define LED_NOTE_OFF_BRIGHTNESS 40
 #define LED_NOTE_ON_BRIGHTNESS 80
 #define LED_CLOCK_BRIGHTNESS 110
+#define MODE_SEPARATE_CHANNELS 0
+#define MODE_POLY 1
+#define MODE_MIRROR 2
+#define MODE_MAX MODE_MIRROR
+#define MODE_DEFAULT MODE_SEPARATE_CHANNELS
+const uint16_t MODE_COLORS_HUE[MODE_MAX + 1] PROGMEM = { 0, 21845, 43691 };
+#define CLK_DIVIDER_MAX 192 // 2 notes
+#define CLK_DIVIDER_MIN 6 // 1/4 note
+#define CLK_DIVIDER_DEFAULT 24
+const uint16_t CLK_DIVIDER_COLORS_VAL[] PROGMEM = { 192, 96, 48, 24, 12, 6 };
+const uint16_t CLK_DIVIDER_COLORS_HUE[] PROGMEM = { 0, 10923, 21845, 32768, 43691, 54613 };
+#define LED_STATE_POSITION_NONE 0
+#define LED_STATE_POSITION_MODE 1
+#define LED_STATE_POSITION_CLK_DIVIDER 2
+#define EEPROM_MODE_ADR 0
+#define EEPROM_CLK_DIVIDER_ADR 1
 
 // DAC variables
 volatile uint8_t *dac_1_port_output, *dac_2_port_output, *dac_3_port_output;
@@ -93,16 +117,31 @@ uint8_t note_number_channel_1, note_number_channel_2;
 // Gate variables
 boolean gate_1_value, gate_2_value;
 
+// Mode variable
+uint8_t mode;
+
 // Clock variables
 uint16_t clock_count;
-uint64_t clock_timeout_timer;
+uint64_t clock_out_timer, clock_timeout_timer;
+uint16_t clock_divider;
 
 // Trig variables
 uint64_t trig_1_timer, trig_2_timer;
-uint64_t led_clock_timer;
 
 // LED variables
-uint16_t note_1_color, note_2_color;
+uint16_t note_1_color, note_2_color, state_color;
+uint64_t led_state_timer;
+uint8_t led_mode_current = 255;
+uint16_t led_clock_divider_current = 65535;
+uint8_t led_divider_color_index;
+uint8_t led_state_position;
+
+// Button variables
+uint64_t button_start_timer;
+boolean button_state;
+boolean button_long_triggered;
+boolean button_short_press, button_long_press;
+boolean button_clk_increment;
 
 // Initialize Adafruit_NeoPixel library
 Adafruit_NeoPixel leds = Adafruit_NeoPixel(2, PIN_WS_LED, NEO_GRB + NEO_KHZ800);
@@ -113,6 +152,8 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 // Voids
 void notes_write(void);
 void led_handler(boolean from_clock = false);
+void button_read(void);
+void button_handle(void);
 void gate_1_write(void);
 void gate_2_write(void);
 void dac_write(void);
@@ -128,6 +169,22 @@ void setup() {
   pinMode(PIN_TRIG_2, OUTPUT);
   pinMode(PIN_GATE_2, OUTPUT);
   pinMode(PIN_WS_LED, OUTPUT);
+  pinMode(PIN_BTN, INPUT_PULLUP);
+  pinMode(PIN_CLK_OUT, OUTPUT);
+
+  // Read settings from EEPROM
+  EEPROM.begin();
+  mode = EEPROM.read(EEPROM_MODE_ADR);
+  if (mode > MODE_MAX) {
+    mode = MODE_DEFAULT;
+    EEPROM.write(EEPROM_MODE_ADR, MODE_DEFAULT);
+  }
+  clock_divider = (EEPROM.read(EEPROM_CLK_DIVIDER_ADR) << 8) | EEPROM.read(EEPROM_CLK_DIVIDER_ADR + 1);
+  if (clock_divider > CLK_DIVIDER_MAX || clock_divider < CLK_DIVIDER_MIN) {
+    clock_divider = CLK_DIVIDER_DEFAULT;
+    EEPROM.write(EEPROM_CLK_DIVIDER_ADR, (clock_divider >> 8) & 0xFF);
+    EEPROM.write(EEPROM_CLK_DIVIDER_ADR + 1, clock_divider & 0xFF);
+  }
 
   // Store ports and masks for fast digital write
   dac_1_port_output = portOutputRegister(digitalPinToPort(PIN_DAC_LATCH_1));
@@ -263,7 +320,7 @@ void setup() {
 
 void loop() {
   // Trig 1 handler
-  if (trig_1_timer != 0 && millis() - trig_1_timer >= TRIG_PULSE_MS) {
+  if (trig_1_timer != 0 && millis() - trig_1_timer >= PULSE_DURATION_MS) {
     // Put TRIG 1 to LOW
     digitalWrite(PIN_TRIG_1, LOW);
     
@@ -272,12 +329,21 @@ void loop() {
   }
 
   // Trig 2 handler
-  if (trig_2_timer != 0 && millis() - trig_2_timer >= TRIG_PULSE_MS) {
+  if (trig_2_timer != 0 && millis() - trig_2_timer >= PULSE_DURATION_MS) {
     // Put TRIG 2 to LOW
     digitalWrite(PIN_TRIG_2, LOW);
     
     // Stop trig timer
     trig_2_timer = 0;
+  }
+
+  // Clock out handler
+  if (clock_out_timer != 0 && millis() - clock_out_timer >= PULSE_DURATION_MS) {
+    // Put CLK to LOW
+    digitalWrite(PIN_CLK_OUT, LOW);
+    
+    // Stop clock timer
+    clock_out_timer = 0;
   }
 
   // Read VCC
@@ -290,6 +356,10 @@ void loop() {
     vcc_prev = vcc;
   }
 
+  // Read and parse button states
+  button_read();
+  button_handle();
+
   // Read data from MIDI
   if (MIDI.read()) {
     switch (MIDI.getType()) {
@@ -299,34 +369,62 @@ void loop() {
         if (MIDI.getChannel() == MIDI_CHANNEL_1) {
           // Get note number
           note_number_channel_1 = MIDI.getData1();
+          if (mode == MODE_MIRROR)
+            note_number_channel_2 = note_number_channel_1;
 
           // Write note to DAC
           notes_write();
 
-          // Start trig
+          // Start trig 1
           digitalWrite(PIN_TRIG_1, HIGH);
           trig_1_timer = millis();
 
-          // Start gate
+          // Start trig 2
+          if (mode == MODE_MIRROR) {
+            digitalWrite(PIN_TRIG_2, HIGH);
+            trig_2_timer = millis();
+          }
+
+          // Start gate 1
           gate_1_value = true;
           gate_1_write();
+
+          // Start gate 2
+          if (mode == MODE_MIRROR) {
+            gate_2_value = true;
+            gate_2_write();
+          }
         }
 
         // Channel 2
         else if (MIDI.getChannel() == MIDI_CHANNEL_2) {
           // Get note number
           note_number_channel_2 = MIDI.getData1();
+          if (mode == MODE_MIRROR)
+            note_number_channel_1 = note_number_channel_2;
 
           // Write note to DAC
           notes_write();
 
-          // Start trig
+          // Start trig 2
           digitalWrite(PIN_TRIG_2, HIGH);
           trig_2_timer = millis();
 
-          // Start gate
+          // Start trig 1
+          if (mode == MODE_MIRROR) {
+            digitalWrite(PIN_TRIG_1, HIGH);
+            trig_1_timer = millis();
+          }
+
+          // Start gate 2
           gate_2_value = true;
           gate_2_write();
+
+          // Start gate 1
+          if (mode == MODE_MIRROR) {
+            gate_1_value = true;
+            gate_1_write();
+          }
         }
         break;
 
@@ -334,16 +432,28 @@ void loop() {
       case midi::NoteOff:
         // Channel 1
         if (MIDI.getChannel() == MIDI_CHANNEL_1 && MIDI.getData1() == note_number_channel_1) {
-          // Stop gate
+          // Stop gate 1
           gate_1_value = false;
           gate_1_write();
+
+          // Stop gate 2
+          if (mode == MODE_MIRROR) {
+            gate_2_value = false;
+            gate_2_write();
+          }
         }
 
         // Channel 2
         if (MIDI.getChannel() == MIDI_CHANNEL_2 && MIDI.getData1() == note_number_channel_2) {
-          // Stop gate
+          // Stop gate 2
           gate_2_value = false;
           gate_2_write();
+
+          // Stop gate 1
+          if (mode == MODE_MIRROR) {
+            gate_1_value = false;
+            gate_1_write();
+          }
         }
         break;
 
@@ -357,16 +467,22 @@ void loop() {
           clock_count = 0;
         clock_timeout_timer = millis();
 
-        // Quarter note (new clock pulse)
-        if (clock_count == 0)
+        // Note (new clock pulse)
+        if (clock_count == 0) {
+          // Start CLK output
+          digitalWrite(PIN_CLK_OUT, HIGH);
+          clock_out_timer = millis();
+
+          // Blink with LED
           led_handler(true);
+        }
 
         // Increment clock counter
         clock_count++;
 
-        // Send pulse once every 24 pulses
-        if (clock_count == 24)
-          clock_count = 0;
+        // Send pulse once every divider pulses
+        clock_count = clock_count % clock_divider;
+
         break;
 
       case midi::ActiveSensing: 
@@ -417,13 +533,50 @@ void notes_write(void) {
  * 
  */
 void led_handler(boolean from_clock) {
-  // Start clock pulse timer
-  if (from_clock)
-    led_clock_timer = millis();
+  // Mode changed
+  if (led_state_position == LED_STATE_POSITION_NONE && mode != led_mode_current) {
+    // Get color
+    state_color = pgm_read_word_near(MODE_COLORS_HUE + mode);
 
-  // Check clock timer
-  if (led_clock_timer != 0 && millis() - led_clock_timer >= TRIG_PULSE_MS)
-    led_clock_timer = 0;
+    // Set state to LED_STATE_POSITION_MODE
+    led_state_position = LED_STATE_POSITION_MODE;
+
+    // Store current value for next loop
+    led_mode_current = mode;
+
+    // Start timer
+    led_state_timer = millis();
+  }
+
+  // Clock divider changed
+  if (led_state_position == LED_STATE_POSITION_NONE && clock_divider != led_clock_divider_current) {
+    // Find color index
+    led_divider_color_index = 0;
+    for (uint8_t i = 0; i < sizeof(CLK_DIVIDER_COLORS_VAL); i++) {
+      if (pgm_read_word_near(CLK_DIVIDER_COLORS_VAL + i) == clock_divider) {
+        led_divider_color_index = i;
+        break;
+      }
+    }
+    
+    // Get color
+    state_color = pgm_read_word_near(CLK_DIVIDER_COLORS_HUE + led_divider_color_index);
+
+    // Set state to LED_STATE_POSITION_CLK_DIVIDER
+    led_state_position = LED_STATE_POSITION_CLK_DIVIDER;
+
+    // Store current value for next loop
+    led_clock_divider_current = clock_divider;
+
+    // Start timer
+    led_state_timer = millis();
+  }
+
+  // Check state timer
+  if (led_state_timer != 0 && millis() - led_state_timer >= LED_SHOW_STATE_DURATION_MS) {
+    led_state_timer = 0;
+    led_state_position = LED_STATE_POSITION_NONE;
+  }
 
   // Calculate note 1 color
   if (note_number_channel_1 >= NOTE_MIN_NUM && note_number_channel_1 <= NOTE_MAX_NUM)
@@ -433,20 +586,112 @@ void led_handler(boolean from_clock) {
   if (note_number_channel_2 >= NOTE_MIN_NUM && note_number_channel_2 <= NOTE_MAX_NUM)
     note_2_color = map((note_number_channel_2 - NOTE_MIN_NUM) % 12, 0, 11, NOTE_COLOR_LOW, NOTE_COLOR_HIGH);
 
-  // Clock pulse -> max led brightness
-  if (led_clock_timer != 0) {
-    leds.setPixelColor(0, leds.gamma32(leds.ColorHSV(note_1_color, 255, LED_CLOCK_BRIGHTNESS)));
-    leds.setPixelColor(1, leds.gamma32(leds.ColorHSV(note_2_color, 255, LED_CLOCK_BRIGHTNESS)));
+  // Show state (mode or clock divider)
+  if (led_state_timer != 0 && led_state_position > 0) {
+    leds.setPixelColor(led_state_position - 1, leds.gamma32(leds.ColorHSV(state_color, 255, LED_CLOCK_BRIGHTNESS)));
+    leds.setPixelColor(1 - (led_state_position - 1), 0);
   }
 
-  // No clock pulse
+  // Default state
   else {
-    leds.setPixelColor(0, leds.gamma32(leds.ColorHSV(note_1_color, 255, gate_1_value ? LED_NOTE_ON_BRIGHTNESS : LED_NOTE_OFF_BRIGHTNESS)));
-    leds.setPixelColor(1, leds.gamma32(leds.ColorHSV(note_2_color, 255, gate_2_value ? LED_NOTE_ON_BRIGHTNESS : LED_NOTE_OFF_BRIGHTNESS)));
+    // Clock pulse -> max led brightness
+    if (from_clock || clock_out_timer != 0) {
+      leds.setPixelColor(0, leds.gamma32(leds.ColorHSV(note_1_color, 255, LED_CLOCK_BRIGHTNESS)));
+      leds.setPixelColor(1, leds.gamma32(leds.ColorHSV(note_2_color, 255, LED_CLOCK_BRIGHTNESS)));
+    }
+
+    // No clock pulse
+    else {
+      leds.setPixelColor(0, leds.gamma32(leds.ColorHSV(note_1_color, 255, gate_1_value ? LED_NOTE_ON_BRIGHTNESS : LED_NOTE_OFF_BRIGHTNESS)));
+      leds.setPixelColor(1, leds.gamma32(leds.ColorHSV(note_2_color, 255, gate_2_value ? LED_NOTE_ON_BRIGHTNESS : LED_NOTE_OFF_BRIGHTNESS)));
+    }
   }
 
   // Update LEDs
   leds.show();
+}
+
+/**
+ * @brief Handles button press and changes mode or clock divider
+ *
+ */
+void button_handle(void) {
+  // Short press
+  if (button_short_press) {
+    // Increment or decrement clock divider
+    if (button_clk_increment)
+      clock_divider *= 2;
+    else
+      clock_divider /= 2;
+
+    // > MAX -> change direction
+    if (clock_divider > CLK_DIVIDER_MAX) {
+      clock_divider = CLK_DIVIDER_MAX / 2;
+      button_clk_increment = false;
+    }
+
+    // < MIN -> change direction
+    if (clock_divider < CLK_DIVIDER_MIN) {
+      clock_divider = CLK_DIVIDER_MIN * 2;
+      button_clk_increment = true;
+    }
+
+    // Write to EEPROM
+    EEPROM.write(EEPROM_CLK_DIVIDER_ADR, (clock_divider >> 8) & 0xFF);
+    EEPROM.write(EEPROM_CLK_DIVIDER_ADR + 1, clock_divider & 0xFF);
+
+    // Clear button press flag
+    button_short_press = false;
+  }
+
+  // Long press
+  if (button_long_press) {
+    // Mode < MAX -> Increment by 1
+    if (mode < MODE_MAX)
+      mode++;
+
+    // Mode >= MAX -> Set to 0
+    else
+      mode = 0;
+
+    // Write to EEPROM
+    EEPROM.write(EEPROM_MODE_ADR, mode);
+
+    // Clear button press flag
+    button_long_press = false;
+  }
+}
+
+/**
+ * @brief Read button state (short / long)
+ *
+ */
+void button_read(void) {
+  // Read button state
+  button_state = !digitalRead(PIN_BTN);
+
+  // Button pressed and no timer -> start timer
+  if (button_state && button_start_timer == 0)
+    button_start_timer = millis();
+
+  // Button pressed, timer > BTN_LONG_DURATION_MS and long press not triggered -> set long press
+  if (button_state && millis() - button_start_timer >= BTN_LONG_DURATION_MS && !button_long_triggered) {
+    button_long_press = true;
+    button_long_triggered = true;
+  }
+
+  // Button not pressed and timer started
+  if (!button_state && button_start_timer != 0) {
+    // No long press triggered and pressed for >= BTN_SHORT_DURATION_MS -> set short press
+    if (!button_long_triggered && millis() - button_start_timer >= BTN_SHORT_DURATION_MS)
+      button_short_press = true;
+
+    // Stop timer
+    button_start_timer = 0;
+
+    // Clear long triggered flag
+    button_long_triggered = false;
+  }
 }
 
 /**
@@ -507,7 +752,7 @@ void vcc_read(void) {
 
     // Restore default ADC state
     analogReference(DEFAULT);
-    analogRead(A0);
+    analogRead(A5);
 
     // Reset timer
     vcc_read_timer = 0;
